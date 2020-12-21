6cc347482732a194e062e736a2d9dc182626af86
irohit427 <irohit427@gmail.com>
2020-12-21 22:42:32 +0530

    Heart Disease Prediction using Keras

--- Heart Disease Prediction/app_v2.py
+++ Heart Disease Prediction/app_v2.py
@@ -4,4 +4,46 @@ Created on Mon Dec 21 21:48:44 2020
 
 @author: rohit
 """
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
+headers = ['age', 'sex','chest_pain','resting_blood_pressure',  
+        'serum_cholestoral', 'fasting_blood_sugar', 'resting_ecg_results',
+        'max_heart_rate_achieved', 'exercise_induced_angina', 'oldpeak',"slope of the peak",
+        'num_of_major_vessels','thal', 'heart_disease']
+
+heart_df = pd.read_csv(r'C:\Users\irohi\Desktop\Deep Learning\ANN\Heart Disease Prediction\data\heart.dat', sep=' ', names=headers)
+X = heart_df.drop(columns=['heart_disease'])
+heart_df['heart_disease'] = heart_df['heart_disease'].replace(1, 0)
+heart_df['heart_disease'] = heart_df['heart_disease'].replace(2, 1)
+
+y = heart_df['heart_disease'].values.reshape(X.shape[0], 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=5)
+
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train = sc.transform(X_train)
+X_test = sc.transform(X_test)
+
+model = Sequential()
+model.add(Dense(30, input_dim=13, activation='tanh'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+#optimizer = Adam(learning_rate=0.001)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=100,verbose=1)
+train_acc = model.evaluate(X_train, y_train, verbose=0)[1]
+test_acc = model.evaluate(X_test, y_test, verbose=0)[1]
+
+print("Train accuracy of keras neural network: {}".format(round((train_acc * 100), 2)))
+print("Test accuracy of keras neural network: {}".format(round((test_acc * 100),2)))
